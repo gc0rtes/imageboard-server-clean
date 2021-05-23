87d612c20fb62c08eb7a5bc89d2a8d5c79ab789b
gc0rtes <guilhermeco@gmail.com>
2021-05-23 10:25:06 +0200

    try/catch added to auth.js login path

--- routers/auth.js
+++ routers/auth.js
@@ -18,34 +18,38 @@ const router = new Router();
 
 //Add SIGNIN ENDPOINT "/login" a POST endpoint to request a JWtoken
 router.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    res.status(400).send("Please supply an email and password");
-  } else {
-    // 1. find user based on email address
-    const user = await User.findOne({
-      where: {
-        email: email,
-      },
-    });
-    if (!user) {
-      res.status(400).send({
-        message: "User with that email does not exist",
-      });
-    }
-    //2. use bcrypt.compareSync to check the received password (first argument) against the stored hash second argument
-    else if (bcrypt.compareSync(password, user.password)) {
-      //3. if the password is correct, return a JWT with the userId of the user (user.id)
-      const jwt = toJWT({ userId: user.id });
-      res.send({
-        jwt,
-      });
+    if (!email || !password) {
+      res.status(400).send("Please supply an email and password");
     } else {
-      res.status(400).send({
-        message: "Password was incorrect",
+      // 1. find user based on email address
+      const user = await User.findOne({
+        where: {
+          email: email,
+        },
       });
+      if (!user) {
+        res.status(400).send({
+          message: "User with that email does not exist",
+        });
+      }
+      //2. use bcrypt.compareSync to check the received password (first argument) against the stored hash second argument
+      else if (bcrypt.compareSync(password, user.password)) {
+        //3. if the password is correct, return a JWT with the userId of the user (user.id)
+        const jwt = toJWT({ userId: user.id });
+        res.send({
+          jwt,
+        });
+      } else {
+        res.status(400).send({
+          message: "Password was incorrect",
+        });
+      }
     }
+  } catch (e) {
+    next(e);
   }
 });
 
